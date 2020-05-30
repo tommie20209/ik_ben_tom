cd18c4aac1ba06541372606e96181912b1990c9c
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-05-30 23:02:35 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,9 +1,71 @@
-module.exports.run = async(bot, message, args) =>{
+const discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
 
-    
-  
-  }
-  
-  module.exports.help ={
+    const cannelID = "700803609029247017";
+
+    var userName = message.author.username;
+    var userDiscriminatoor = message.author.discriminator;
+
+    var ticketBestaat = false;
+
+
+    message.guild.channels.cache.forEach(element => {
+        if (channel.name == userName.tolowerCase() + "-" + userDiscriminatoor) {
+            ticketBestaat = true;
+
+            message.reply("je hebt al een ticket gemaakt");
+
+            return;
+        }
+
+    });
+
+    if (ticketBestaat) return;
+
+    var embed = new discord.MessageEmbed()
+        .setTitle("hoi" + message.author.username)
+        .setFooter("ticket wordt gemaakt");
+
+    message.channel.send(embed);
+
+
+    message.guild.channels.create(username.tolowerCase() + "-" + userDiscriminatoor, { type: "text" }).then(
+        (createdchannel) => {
+            createdchannel.setParent(cannelID).then(
+                (setedparent) => {
+
+                    setedparent.updateOverwrite(message.guild.roles.cache.find(r => r.name == `@welkom`), {
+                        SEND_MESSAGES: false,
+                        VIEW_CHANNEL: false
+                    });
+
+                    setedparent.updateOverwrite(message.guild.roles.cache.find(r => r.name == `@everyone`), {
+                        SEND_MESSAGES: false,
+                        VIEW_CHANNEL: false
+                    });
+
+                    setedparent.updateOverwrite(message.author.id, {
+                        SEND_MESSAGES: true,
+                        VIEW_CHANNEL: true,
+                        ATTACH_FILES: true,
+                        CREATE_INSTANT_INVITE: false,
+                        ADD_REACTIONS: true
+                    });
+
+                    var embedtt = new discord.MessageEmbed()
+                        .setTitle(`hoi ${message.author.username}`);
+                   setedparent.send(embedtt);
+                }
+            ).catch(err => {
+                message.channel.send("er is misgegaan");
+            });
+        }
+    ).catch(err => {
+        message.channel.send("er is misgegaan");
+    });
+
+}
+
+module.exports.help = {
     name: "ticket"
-  }
+}
