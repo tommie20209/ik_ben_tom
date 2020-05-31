a49c8f842c809ad3867ce88ce71799d4a5827a53
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-05-31 22:28:01 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,26 +1,5 @@
-const discord = require("discord.js");
-const botConfig = require("./botconfig.json");
-const fs = require("fs");
 module.exports.run = async(bot, message, args) =>{
-  fs.readdir("./commands/", (err, files) => {
-
-    if (err) console.log(err);
-
-    var jsFiles = files.filter(f => f.split(".").pop() === "help.name");
-
-    if (jsFiles.length <= 0) {
-        console.log("kon geen files vinden");
-        return;
-    }
-
-    jsFiles.forEach((f, i) => {
-
-        var fileGet = require(`./commands/${f}`);
-        console.log(`de file ${f} is geladen`);
-
-        client.commands.set(fileGet.help.name, fileGet);
-    })
-});
+  
     try{
         
     var textt = "**toms bot** \n\n **_commands_** \n !hallo - geeft hallo terug. \n !doei - geeft doei terug. \n !info - Geeft info \n !serverinfo - geef server info. \n\n\n **_admin commands_** \n !ban - om iemand te bannen \n !kick - kick iemand";
