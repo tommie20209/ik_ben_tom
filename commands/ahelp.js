15bf497043b3e47c4137011ef4f152d466e3a949
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-01 17:36:51 +0200

    ew4s

---
+++ commands/ahelp.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+module.exports.run = async(bot, message, args) =>{
+
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return;
+
+    var helptext = "**_admin commands_** \n !ban - om iemand te bannen \n !kick - kick iemand \n !tempmute - mute een speler voor een bebaalde tijd";
+
+    message.author.send(helptext);
+  }
+  
+  module.exports.help ={
+    name: "hallo"
+  }
--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ module.exports.run = async(bot, message, args) =>{
   
     try{
         
-    var textt = "**toms bot** \n\n **_commands_** \n !hallo - geeft hallo terug. \n !doei - geeft doei terug. \n !info - Geeft info \n !serverinfo - geef server info. \n\n\n **_admin commands_** \n !ban - om iemand te bannen \n !kick - kick iemand";
+    var textt = "**toms bot** \n\n **_commands_** \n !hallo - geeft hallo terug. \n !doei - geeft doei terug. \n !info - Geeft info \n !serverinfo - geef server info \n !drops - geeft een random drop aan voor fn \n !ticket - maak een ticket. ";
 
     message.author.send(textt);
 
