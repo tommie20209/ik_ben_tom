dc1bece90a65a4551e90758c617876cabb8266d2
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-08 09:34:32 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -27,7 +27,7 @@ module.exports.run = async(bot, message, args) =>{
       category: command.help.category
     }
  
-    commandlist.push(constructor)
+    commandlist.push(constructor);
      
    });
 
