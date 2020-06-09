20baf765a5d05e43147063f4e01cd1a90cf18770
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-09 11:18:06 +0200

    d2

--- commands/kick.js
+++ commands/kick.js
@@ -84,5 +84,5 @@ module.exports.run = async(bot, message, args) =>{
 module.exports.help ={
   name: "kick",
   description: "kick een speler",
-  category: "_ADMIN COMMANDS_"
+  category: "algemeen"
 }
