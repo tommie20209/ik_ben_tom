2bb949a66fe1f66944005780634b016b264dc9ef
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-16 15:07:47 +0200

    QAs

---
+++ commands/test.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+
+    var testembed = new discord.MessageEmbed()
+        .setTitle("test")
+        .addFields(
+            { name: "test", value: message.author.username}
+        )
+     return message.channel.send(testembed);
+}
+
+module.exports.help = {
+    name: "test",
+    description: "geeft hallo",
+    category: "algemeen"
+}
