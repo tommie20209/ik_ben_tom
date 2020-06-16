00eaf5ac5296c9c3d91b3357fd50477d7b250f32
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-16 11:19:03 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,19 +2,7 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const botConfig = require("../botconfig.json");
 module.exports.run = async(bot, message, args) =>{
-  
-//     try{
-        
-//     var textt = `**toms bot** \n\n **_commands_** \n !hallo - geeft hallo terug. \n !doei - geeft doei terug. \n !info - Geeft info \n !serverinfo - geef server info \n !drops - geeft een random drop aan voor fn \n !ticket - maak een ticket. `;
 
-//     message.author.send(textt);
-
-//     message.reply("alle command staan in je prive berichten📫👍.");  
-
-
-//     }catch(error){
-//         message.reply("Er is iets misgeaan");
-//     
 
    var commandlist = [];
    var prefix = botConfig.prefix;
