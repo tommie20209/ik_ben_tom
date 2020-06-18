30290720d06e103dc291c0bdca54276f9d59c500
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-18 16:47:37 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,55 +1,54 @@
 const discord = require("discord.js");
-const botConfig =  require("../botconfig.json");
-module.exports.run = async(bot, message, args) =>{
-var commandsList = [];
+const botConfig = require("../botconfig.json");
+module.exports.run = async (bot, message, args) => {
+  var commandsList = [];
 
-    bot.commands.forEach(command => {
+  bot.commands.forEach(command => {
 
-        var item = {
+    var item = {
 
-            name: command.help.name,
-            description: command.help.description,
-            category: command.help.category
+      name: command.help.name,
+      description: command.help.description,
+      category: command.help.category
 
-        }
+    }
 
-        commandsList.push(item);
+    commandsList.push(item);
 
-    });
+  });
 
-    var commandlist;
-    var prefix = botConfig.prefix;
-    var response = "";
+  var commandlist;
+  var prefix = botConfig.prefix;
+  var response = "";
 
 
-    for (let i = 0; i < commandsList.length; i++) {
-      const command = commandlist[i];  
-      if(command["category"] == "algemeen"){
-  
-       general +=`${prefix}${command["name"]} - ${command["description"]}\n`;
-  
-  
-      }else if(command["category"] == "infomatie"){
-  
-       infomatie +=`${prefix}${command["name"]} - ${command["description"]}\n`;
-  
+  for (var i = 0; i < commandsList.length; i++) {
+    const command = commandlist[i];  
+    if(command["category"] == "algemeen"){
+      general += `${prefix}${command["name"]} - ${command["description"]}\n`;
 
-        
+
+    } else if (command["category"] == "infomatie") {
+
+      infomatie += `${prefix}${command["name"]} - ${command["description"]}\n`;
+
+
+
 
     }
   }
-    message.author.send(response).then(() => {
+  message.author.send(response).then(() => {
 
-        message.channel.send("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
+    message.channel.send("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
 
-    }).catch(() => {
+  }).catch(() => {
 
-        message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen");
+    message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen");
 
-    });
+  });
 
 }
 
 module.exports.help = {
-           name: "help"
+  name: "help"
 }
