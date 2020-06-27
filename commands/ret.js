e746ab53e41ba539890e281832fb3cbd7c543924
tommie20209 <63231830+tommie20209@users.noreply.github.com>
2020-06-27 22:38:25 +0200

    Create ret.js

---
+++ commands/ret.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+module.exports.run = async(bot, message, args) =>{
+
+  }
+  
+  module.exports.help ={
+    name: "hallo",
+    description: "geeft hallo",
+    category: "algemeen"
+  }
